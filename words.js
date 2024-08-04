edba57a0c9c5632f0392d8e5225e7f9b974238a0
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-08-04 19:12:48 +0000

    update

--- words.js
+++ words.js
@@ -1,11 +1,107 @@
 const words = [
   {
-    word: "apple",
-    hint: "A fruit that grows on trees"
+    word: "Invisible",
+    hint: "She don't even know you"
   },
   {
-    word: "addiction",
-    hint: "A state of extreme addiction"
-  }
+    word: "Fearless",
+    hint: "Run your hairs through your hair"
+  },
+  {
+    word: "Fifteen",
+    hint: "Take a deep breath, go"
+  },
+  {
+    word: "Change",
+    hint: "And well sing hulleigyah"
+  },
+  {
+    word: "Untouchable",
+    hint: "Its like a million little stars spelling out your name"
+  },
+  {
+    word: "Superstar",
+    hint: "You smile that beautiful smile"
+  },
+  {
+    word: "Mine",
+    hint: "I can see it now"
+  },
+  {
+    word: "mean",
+    hint: "Well you can take me down"
+  },
+  {
+    word: "enchanted",
+    hint: "This was the very first page"
+  },
+  {
+    word: "ours",
+    hint: "But this love is ours"
+  },
+  {
+    word: "Superman",
+    hint: "Go save the world, I'll be around"
+  },
+  {
+    word: "timeless",
+    hint: "So even in a different life"
+  },
+  {
+    word: "red",
+    hint: "Missing him was dark grey, all alone"
+  },
+  {
+    word: "treacherous",
+    hint: "this slope is reckless"
+  },
+  {
+    word: "ronan",
+    hint: "you were my best four years"
+  },
+  {
+    word: "babe",
+    hint: "Im here on the kitchen floor"
+  },
+  {
+    word: "run",
+    hint: "I'll wait for you"
+  },
+  {
+    word: "style",
+    hint: "Takes me home"
+  },
+  {
+    word: "clean",
+    hint: "ten months sober"
+  },
+  {
+    word: "delicate",
+    hint: "I pretend your mine all the damn time"
+  },
+  {
+    word: "Gorgeous",
+    hint: "Unless you wanna come along?"
+  },
+  {
+    word: "dress",
+    hint: "carve you name into my bedpost"
+  },
+  {
+    word: "lover",
+    hint: "Can i go where you go?"
+  },
+  {
+    word: "Afterglow",
+    hint: "Hey, its all me, in my head"
+  },
+  {
+    word: "Me!",
+    hint: "Hey kids spellings fun!"
+  },
+  {
+    word: "daylight",
+    hint: "Back and forth from new york."
+  },
   
 ]
